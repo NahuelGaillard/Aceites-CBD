a6dc8af99ad6acca2500f32559667e578277641c
NahuelGaillard <nahuelgaillard18@hotmail.com>
2021-11-08 16:03:44 -0300

    Arreglo productos

--- stockProductos.js
+++ stockProductos.js
@@ -1,25 +1,25 @@
 let Productos = [
-    {img:'media/productos/img1.jpg', nombre:"Gotero Royal 10ml", valor: 2200, descripcion:"39.99% de CBD"},
-    {img:'media/productos/img2.jpg', nombre:"Gotero Royal 30ml", valor: 4350, descripcion:"39.99% de CBD"},
-    {img:'media/productos/img3.jpg', nombre:"Gotero Royal 10ml Red", valor: 1550, descripcion:"Fortalece el sistema inmunológico 10% CBD"},
-    {img:'media/productos/img4.jpg', nombre:"Crema Royal 50gr", valor: 2000, descripcion:"39.9% cbd"},
-    // {img:'media/productos/img5.jpeg', nombre:"Royal extra fuerte con THC", valor: 2900 , descripcion:"Envase de 50 gramos"},
-    {img:'media/productos/img6.jpg', nombre:"Ungüento de Peru 90gr", valor: 950, descripcion:"Envase de 90 gramos"},
-    {img:'media/productos/img7.jpeg', nombre:"Harina sativa", valor: 300, descripcion:"Paquete de 100 gramos"},
-    {img:'media/productos/img8.jpeg', nombre:"Aceite de THC Naranja 10ml", valor: 3950, descripcion:"Ratio 1a1: 50% de indica y 50% sativa."},
-    {img:'media/productos/img9.jpeg', nombre:"Aceite de THC Amarillo 10ml", valor: 3950, descripcion:"Ratio 3a1: 80% indica y 20% sativa."},
-    {img:'media/productos/img10.jpeg', nombre:"Aceite de THC Verde 10ml", valor: 3950, descripcion:"Ratio 2a1: 70% de sativa y 30% indica"},
-    {img:'media/productos/img11.jpeg', nombre:"Serum facial 20gr", valor: 2800, descripcion:"Acido hialurónico"},
-    {img:'media/productos/img12.jpeg', nombre:"Serum facial vitamica C 20gr", valor: 2800, descripcion:"Vitamina C y Acido hialirónico"},
-    {img:'media/productos/img13.jpg', nombre:"Fish CBD oil", valor: 4800, descripcion:"Envase 60 cápsulas blandas"},
-    {img:'media/productos/img14.jpeg', nombre:"Gummies CBD", valor: 4000, descripcion:"Envase de 50 unidades, 15mg de CBD por unidad"},
-    {img:'media/productos/img15.jpeg', nombre:"Gummies CBD acidas", valor: 4000, descripcion:"Envase de 50 unidades acidas, 15mg de CBD por unidad"},
-    // {img:'media/productos/img16.jpeg', nombre:"Royal PET 10ml", valor: 2200, descripcion:"Gotero de 10 ml, sabor carne."},
-    {img:'media/productos/img17.jpeg', nombre:"Royal PET 30ml", valor: 4350, descripcion:"Gotero de 30 ml, sabor carne."},
-    {img:'media/productos/img18.jpeg', nombre:"Jabon Royal 110gr", valor: 500, descripcion:"39.9% CBD"},
-    {img:'media/productos/img19.jpeg', nombre:"CAFE+CBD", valor: 1250, descripcion:"#"},
-    {img:'media/productos/img20.jpeg', nombre:"CAFE+LIQUIDO DE VAPEO", valor: 4500, descripcion:"#"},
-    // {img:'media/productos/img21.jpeg', nombre:"LINEA ROOT", valor: 3950, descripcion:"#"},
+    {img:'media/productos/img1.jpg', nombre:"Gotero Royal 10ml", descripcion:"El Aceite Sublingual Calidad Premium Royal Importado 39,9 % de CBD,Uno de los aceites con mas porcentaje de CBD del mercado, es un producto 100% medicinal con propiedades curativas increíbles. Libre de THC lo que lo hace apto para todo tipo de edades. Disponible tambien en su tamaño de 30ml"},
+    // {img:'media/productos/img2.jpg', nombre:"Gotero Royal 30ml", descripcion:"El Aceite Sublingual Calidad Premium Royal Importado 39,9 % de CBD,Uno de los aceites con mas porcentaje de CBD del mercado, es un producto 100% medicinal con propiedades curativas increíbles. Libre de THC lo que lo hace apto para todo tipo de edades."},
+    {img:'media/productos/img3.jpg', nombre:"Gotero Royal 10ml Red", descripcion:"Aceite Sublingual Rojo CBD de 10% (cáñamo) ROYAL un producto 100% medicinal con propiedades curativas increíbles, Fortalece el sistema inmunológico. Posee una concentracion de 10% CBD-200MG"},
+    {img:'media/productos/img4.jpg', nombre:"Crema Royal 50gr", descripcion:"Crema Corporal Royal CBD - Importada 39,9% de CBD 50gr. Es muy efectiva en los dolores de origen muscular o reumático, y en cuadros dermatológicos como urticarias, dermatitis o psoriasis. Tiene propiedades relajantes, antiinflamatorias y antioxidantes."},
+    // {img:'media/productos/img5.jpeg', nombre:"Royal extra fuerte con THC", descripcion:"Envase de 50 gramos"},
+    {img:'media/productos/img6.jpg', nombre:"Ungüento de Peru 90gr",  descripcion:"Envase de 90 gramos"},
+    {img:'media/productos/img7.jpeg', nombre:"Harina sativa", descripcion:"Paquete de 100 gramos"},
+    {img:'media/productos/img8.jpeg', nombre:"Aceite de THC Naranja 10ml", descripcion:"Ratio 1a1: 50% de indica y 50% sativa."},
+    {img:'media/productos/img9.jpeg', nombre:"Aceite de THC Amarillo 10ml", descripcion:"Ratio 3a1: 80% indica y 20% sativa."},
+    {img:'media/productos/img10.jpeg', nombre:"Aceite de THC Verde 10ml", descripcion:"Ratio 2a1: 70% de sativa y 30% indica"},
+    {img:'media/productos/img11.jpeg', nombre:"Serum facial 20gr", descripcion:"El Serum Facial Royal Con CBD Y Acido Hialuronico , Brinda Un Efecto Anti-Age , Calmante Y Refrescante , Protegiendo A La Piel De Los Agresores Externos Que La Dañan Y Envejecen , Dando Un Aspecto Joven Y Saludable , Estimulando La Producción De Colágeno Tonificando Y Hidratando El Rostro Y Cuello"},
+    {img:'media/productos/img12.jpeg', nombre:"Serum facial vitamica C 20gr", descripcion:"El Serum Facial Royal Con CBD Incorpora Además Los Beneficios De La Vitamina C , Que No Solo Aporta Luminosidad, Sino Que También Ayuda A Eliminar Las Manchas Solares , Las Cicatrices , Combate Líneas De Expresión , Aportando Tersura Y Efecto Glow Al Rostro Y Cuello"},
+    {img:'media/productos/img13.jpg', nombre:"Fish CBD oil", descripcion:"Las cápsulas de CBD son una de las formas más convenientes y discretas de tomar su dosis de CBD. Cuando el CBD se ingiere en forma de cápsulas, tiene que pasar por alto el sistema digestivo y el hígado antes de que el compuesto de CBD se absorba en el torrente sanguíneo. Debido al proceso de derivación, los efectos de las cápsulas de CBD pueden tardar más en surtir efecto. Además, la cantidad de CBD que llega al torrente sanguíneo se reduce en comparación con las gotas de aceite de CBD debajo de la lengua. Envase 60 cápsulas blandas."},
+    {img:'media/productos/img14.jpeg', nombre:"Gummies CBD", descripcion:"Envase de 50 unidades acidas,Contienen 15 mg de CBD, ayuda a aprovechar los numerosos beneficios de salud y bienestar del CBD."},
+    {img:'media/productos/img15.jpeg', nombre:"Gummies CBD acidas", descripcion:"Envase de 50 unidades acidas,Contienen 15 mg de CBD, ayuda a aprovechar los numerosos beneficios de salud y bienestar del CBD."},
+    {img:'media/productos/img16.jpg', nombre:"Royal PET 10ml", descripcion:"Ahora también tus mascotas pueden beneficiarse de los efectos positivos del cbd. el cbd puede tener un efecto antiinflamatorio, antiepiléptico y antioxidante en los animales y puede reducir el dolor y el estrés, mejorando así la salud y el bienestar de su amigo de cuatro patas. Concentración: 15% de CBD, 125MG. Libre de THC"},
+    {img:'media/productos/img17.jpeg', nombre:"Royal PET 30ml", descripcion:"Ahora también tus mascotas pueden beneficiarse de los efectos positivos del cbd. el cbd puede tener un efecto antiinflamatorio, antiepiléptico y antioxidante en los animales y puede reducir el dolor y el estrés, mejorando así la salud y el bienestar de su amigo de cuatro patas. Concentración: 15% de CBD, 125MG. Libre de THC"},
+    {img:'media/productos/img18.jpeg', nombre:"Jabon Royal 110gr", descripcion:"Con aceite de semilla de cañamo 100% puro. Nutritivo y humectante por su contenido de omega 3 y 6. Para uso diario. Auxiliar en cuidado de la piel con acné, ideal para la piel grasa y seca, elimina las toxinas de la epidermis, activa la circulación, posee propiedades antiinflamatorias. No contiene aromas artificiales"},
+    {img:'media/productos/img19.jpeg', nombre:"Cafe Royal Premium 300MG x100GR", descripcion:"El CBD mejora la alerta mental inducida por la cafeína en el café y reduce los molestos efectos secundarios como la ansiedad y el nerviosismo. Aquí le decimos cómo mezclar CBD con café. La mezcla de CBD y café está de moda en el mundo del CBD en estos días. Incluso puede comprar granos de café que vienen pre-mezclados con CBD. Más que una simple tendencia de marketing, hay varias ventajas al hacer esto: los efectos relajantes del CBD ayudan a reducir los efectos secundarios no deseados (inquietud y ansiedad) del consumo de cafeína. Además de esto, incluso se informa que el CBD mejora el estado de alerta inducido por el café y prolonga los efectos de ambos compuestos: es una combinación hecha en el cielo."},
+    {img:'media/productos/img20.jpeg', nombre:"E-Liquid Royal Para Vapeo 30ml", descripcion:"Aceite de CBD vaporizado. Durante la vaporización, el CBD llega rápidamente a los pulmones y se difunde directamente por el torrente sanguíneo en el lugar de pasar por el intestino y el hígado. Esto evita por completo el efecto del primer paso, permitiendo que casi cuatro veces más de CBD entre en el sistema circulatorio y aumente la biodisponibilidad al 50-60%. En esencia, lo que esto significa es que se pueden lograr los mismos efectos vaporizando que consumiendo por vía oral, pero usando mucha menos cantidad de CBD."},
+    // {img:'media/productos/img21.jpeg', nombre:"LINEA ROOT", descripcion:"#"},
 
 ]
 
@@ -31,16 +31,14 @@ Productos.forEach((prod) => {
     div.className = "promos, m-1"
     
     div.innerHTML = `
-                <div class="card" style="width: 18rem;">        
-                    <img src="${prod.img}" class="card-img-top" alt="${prod.nombre}">
-                    <div class="card-body">
-                        <h5 class="card-title type-bold">${prod.nombre}</h5>
-                        <hr>
-					    <p class="card-text text-center">Precio: $${prod.valor}</p>
-                        <hr>
-                        <p class="card-text">${prod.descripcion}</p>
+                    <div class="row">        
+                        <img src="${prod.img}" class="card-img-top col-12 col-md-3" alt="${prod.nombre}">
+                        <div class="card-body col-12 col-md-8" >
+                            <h5 class="card-title type-bold">${prod.nombre}</h5>
+                            <hr>   
+                            <p class="card-text">${prod.descripcion}</p>
+                        </div>
                     </div>
-                </div>
                     `
     
     cardProductos.appendChild(div)
