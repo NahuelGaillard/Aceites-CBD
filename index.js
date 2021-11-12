cf233da6a8c0ded8e73041328a03c10efd2af641
NahuelGaillard <nahuelgaillard18@hotmail.com>
2021-11-12 14:13:51 -0300

    wpp, destacados y info1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var promoProductos = [
-    {img:'media/productos/promo1.jpeg', nombre:"Promo 1", valor: "$4000 c/u", descripcion:"Llevando 2 aceites de 30 ML (vale pet)"},
-    {img:'media/productos/promo2.jpeg', nombre:"Promo 2", valor: "700 c/u", descripcion:"Llevando 2 ungüentos"},
-    {img:'media/productos/promo3.jpeg', nombre:"Promo 3", valor: "2000 c/u", descripcion:"Llevando 2 aceies de 10ml"},
-    {img:'media/productos/promo4.jpeg', nombre:"Promo 4", valor: "4500 c/u", descripcion:"Llevando 2 fish CBD oil"},
-    {img:'media/productos/promo5.jpeg', nombre:"Promo 5", valor: "3750 c/u", descripcion:"Llevando 2 Gummies CBD"},
+    {img:'media/productos/img1.jpg', nombre:"Gotero Royal 10ml", descripcion:"El Aceite Sublingual Calidad Premium Royal Importado 39,9 % de CBD, Disponible tambien en su tamaño de 30ml"},
+    {img:'media/productos/img6.jpg', nombre:"Ungüento de Peru 90gr",  descripcion:"Envase de 90 gramos"},
+    {img:'media/productos/img8.jpeg', nombre:"Aceite de THC Naranja 10ml", descripcion:"Ratio 1a1: 50% de indica y 50% sativa."},
+    {img:'media/productos/img14.jpeg', nombre:"Gummies CBD", descripcion:"Envase de 50 unidades acidas,Contienen 15 mg de CBD, ayuda a aprovechar los numerosos beneficios de salud y bienestar del CBD."},
+    {img:'media/productos/img15.jpeg', nombre:"Gummies CBD acidas", descripcion:"Envase de 50 unidades acidas, Contienen 15 mg de CBD"},
 
 ]
 
@@ -17,10 +17,9 @@ promoProductos.forEach((promo) => {
     
     div.innerHTML = `
     <div class="card mb-3">
-        <h5 class="card-title type-bold">${promo.nombre}</h5>
+        <h5 class="card-title p-1">${promo.nombre}</h5>
         <img src="${promo.img}" class="card-img-top" alt="${promo.nombre}">
-        <div class="card-body color-black ">
-            <p class="card-text text-center type-bold">Precio: $${promo.valor}</p>
+        <div class="card-body type-sm color-black">
             <p class="card-text">${promo.descripcion}</p>
         </div>
     </div>
