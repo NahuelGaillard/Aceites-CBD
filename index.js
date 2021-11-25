d4a6a4dffb07bd283015d5ffc50a5c6412ab912f
NahuelGaillard <nahuelgaillard18@hotmail.com>
2021-11-25 12:27:50 -0300

    Cambio de imagenes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var promoProductos = [
     {img:'media/productos/img1.jpg', nombre:"Gotero Royal 10ml", descripcion:"El Aceite Sublingual Calidad Premium Royal Importado 39,9 % de CBD, Disponible tambien en su tamaño de 30ml"},
     {img:'media/productos/img6.jpg', nombre:"Ungüento de Peru 90gr",  descripcion:"Envase de 90 gramos"},
-    {img:'media/productos/img8.jpeg', nombre:"Aceite de THC Naranja 10ml", descripcion:"Ratio 1a1: 50% de indica y 50% sativa."},
-    {img:'media/productos/img14.jpeg', nombre:"Gummies CBD", descripcion:"Envase de 50 unidades acidas,Contienen 15 mg de CBD, ayuda a aprovechar los numerosos beneficios de salud y bienestar del CBD."},
-    {img:'media/productos/img15.jpeg', nombre:"Gummies CBD acidas", descripcion:"Envase de 50 unidades acidas, Contienen 15 mg de CBD"},
+    {img:'media/productos/img8.jpg', nombre:"Aceite de THC Naranja 10ml", descripcion:"Ratio 1a1: 50% de indica y 50% sativa."},
+    {img:'media/productos/img14.jpg', nombre:"Gummies CBD", descripcion:"Envase de 50 unidades acidas,Contienen 15 mg de CBD, ayuda a aprovechar los numerosos beneficios de salud y bienestar del CBD."},
+    {img:'media/productos/img15.jpg', nombre:"Gummies CBD acidas", descripcion:"Envase de 50 unidades acidas, Contienen 15 mg de CBD"},
 
 ]
 
